bf3bbf826ac04ffb040d0fed9f5e44d45791647d
rismakov <rismakov@gmail.com>
2018-09-15 15:06:00 -0700

    add constants file

---
+++ constants.py
@@ -0,0 +1,24 @@
+COLORS = [
+    'salmon', 'salmon', 'salmon', 'salmon',
+    'mediumaquamarine', 'mediumaquamarine', 'mediumaquamarine', 'mediumaquamarine',
+    'orange', 'orange', 'orange', 'orange',
+    'skyblue', 'skyblue', 'skyblue', 'skyblue',
+    'silver', 'silver', 'silver', 'silver'
+]
+
+COLOR_INCREASE = {
+    'salmon': 'orangered',
+    'mediumaquamarine': 'teal',
+    'orange': 'chocolate',
+    'skyblue': 'dodgerblue',
+    'silver': 'gray'
+}
+
+LETTER_TYPES = ['E_I', 'N_S', 'F_T', 'J_P']
+
+PERSONALITY_DIMS = [
+        'Extraversion-Introversion', 
+        'Intuition-Sensing', 
+        'Feeling-Thinking', 
+        'Judging-Perceiving'
+]
