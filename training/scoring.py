54f0aeddf82e289bc760775f5a61c45fc0b6c81f
rismakov <rismakov@gmail.com>
2019-01-18 16:42:00 -0800

    fix up scoring functions

--- training/scoring.py
+++ training/scoring.py
@@ -3,65 +3,66 @@ from sklearn.metrics import (accuracy_score, f1_score,
 
 from constants import LETTER_TYPES, PERSONALITY_DIMS
 
-
-def get_precision_recall_f1(true, pred, average_type):
-    metrics = {}
-    metrics['precision'] = precision_score(true, pred, average=average_type)
-    metrics['recall'] = recall_score(true, pred, average=average_type)
-    metrics['f1'] = f1_score(true, pred, average=average_type)
-    return metrics
+AVERAGE_TYPE = 'macro'
 
 
-def get_multiclass_metrics(true, pred, average_types):
+def get_multiclass_metrics(true, pred):
+    '''Returns dict of all metrics (accuracy, precision, recall, and f1).
+
+    :param true type: list or array-type of true labels.
+    :param pred type: list of array-type of predicted labels.
+
+    :returns type: dict of floats with keys: ['accuracy', 'precision', 'recall', and 'f1'].
+    '''
     metrics = {}
     metrics['accuracy'] = accuracy_score(true, pred)
-
-    for average_type in average_types:
-        metrics[average_type] = get_precision_recall_f1(true, pred, average_type)
+    metrics['precision'] = precision_score(true, pred, average=AVERAGE_TYPE)
+    metrics['recall'] = recall_score(true, pred, average=AVERAGE_TYPE)
+    metrics['f1'] = f1_score(true, pred, average=AVERAGE_TYPE)
 
     return metrics
 
 
-def get_metrics_for_each_letter(
-    true, pred, return_dim=None, first_level=None, second_level=None, average_types=['macro']
+def scorer_func_for_each_letter(
+    true, pred, dim, scoring_type, average_types=['macro']
 ):
+    '''Returns score from scoring function given personality dimension and scoring type.
+    '''
     metrics = {}
     for i, dim in enumerate(PERSONALITY_DIMS):
         true_dim = [x[i]for x in true]
         pred_dim = [x[i]for x in pred]
 
-        metrics[dim] = get_multiclass_metrics(true_dim, pred_dim, average_types=average_types)
+        metrics[dim] = get_multiclass_metrics(true_dim, pred_dim)
 
-    if return_dim and first_level and second_level:
-        return metrics[return_dim][first_level][second_level]
-    elif return_dim and first_level:
-        return metrics[return_dim][first_level]
-    else:
-        return metrics
+    return metrics[dim][scoring_type]
 
 
 def get_metrics_for_letter_type(scoring, dim, letter_type):
+    '''Iterates through every scoring_type to grab result from scoring function.
+    '''
     scoring_types = ['accuracy', 'precision', 'recall', 'f1']
-    first_levels = ['accuracy', 'macro', 'macro', 'macro']
-    second_levels = [None, 'precision', 'recall', 'f1']
 
-    for scoring_type, first_level, second_level in zip(scoring_types, first_levels, second_levels):
-        scoring[letter_type + '_' + scoring_type] = make_scorer(get_metrics_for_each_letter, return_dim=dim, 
-                                                                first_level=first_level, second_level=second_level, 
-                                                                greater_is_better=True)
+    for scoring_type in scoring_types:
+        key_name = letter_type + '_' + scoring_type
+        scoring[key_name] = make_scorer(
+            scorer_func_for_each_letter, dim=dim, scoring_type=scoring_type, greater_is_better=True
+        )
 
     return scoring
 
 
 def get_comprehensive_scoring_types():
+    '''Iterates through every MBTI Personality dimension to grab result from scoring function.
+    '''
     scoring = {
-        'accuracy': make_scorer(accuracy_score), 
-        'precision': make_scorer(precision_score, average='macro'), 
-        'recall': make_scorer(recall_score, average='macro'),
-        'f1': make_scorer(f1_score, average='macro'),
+        'accuracy': make_scorer(accuracy_score),
+        'precision': make_scorer(precision_score, average=AVERAGE_TYPE),
+        'recall': make_scorer(recall_score, average=AVERAGE_TYPE),
+        'f1': make_scorer(f1_score, average=AVERAGE_TYPE),
     }
 
-    # get metrics for each letter type (i.e. E-I, N-S, F-T, J-P)
+    # add metrics for each letter type (i.e. E-I, N-S, F-T, J-P) to full type metrics
     for dim, letter_type in zip(PERSONALITY_DIMS, LETTER_TYPES):
         scoring = get_metrics_for_letter_type(scoring, dim, letter_type)
 
