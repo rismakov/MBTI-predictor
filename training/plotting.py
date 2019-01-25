cbe8d146d4d3d747d08207ecc6aedb506bf59338
rismakov <rismakov@gmail.com>
2019-01-25 01:22:04 -0800

    refactor plotting funds

--- training/plotting.py
+++ training/plotting.py
@@ -1,25 +1,24 @@
 from __future__ import division, print_function
 
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+
 import numpy as np
 import pandas as pd
 
-color_mappings = {
+from utils.constants import (
+    FEATURIZED_PATH, FUNCTION_PLOTS_PATH, LABEL_COL, MBTI_TYPES_UPPER, PLOTS_PATH, TOP_WORDS, VECTORIZED_PATH
+)
+
+COLOR_MAPPINGS = {
     'EN': 'dodgerblue',
     'ES': 'slategray',
     'IN': 'lightseagreen',
     'IS': 'salmon',
-    'E': 'orangered',
-    'I': 'forestgreen',
-    'N': 'c',
-    'S': 'lightpink',
-    'T': 'mediumseagreen',
-    'F': 'mediumpurple',
-    'J': 'gray',
-    'P': 'chocolate'
 }
 
-type_mappings = {
+FUNCTION_MAPPINGS = {
     'EN': 'Extraversion-Intuition',
     'ES': 'Extraversion-Sensing',
     'IN': 'Introversion-Intuition',
@@ -34,6 +33,17 @@ type_mappings = {
     'P': 'Perceiving'
 }
 
+FUNCTION_INDS = {
+    'EI': [0, 1],
+    'NS': [1, 2],
+    'FT': [2, 3],
+    'JP': [3, 4],
+    '1_2': [0, 2],
+    '2_3': [2, 3]
+}
+
+
+plt.style.use('fivethirtyeight')
 
 def label_fig_xaxis_names(names, width=0):
     x_range = np.arange(len(names))
@@ -41,16 +51,23 @@ def label_fig_xaxis_names(names, width=0):
     plt.xlim([x_range.min()-0.5, x_range.max()+0.5 + width])
 
 
+def get_marker_color(mbti_type):
+    '''If looking at full MBTI type, returns color depending on value or COLOR_MAPPINGS when keying by the first two 
+    functions. Otherwise returns 'black' if not looking at the full MBTI type.
+    '''
+    if len(mbti_type) == 4:
+        return COLOR_MAPPINGS[mbti_type[:2]]
+    
+    return 'black'
+
+
 def errorbar_graph_of_mean_feature_values(means, errs, xaxis_names, feature):
     for i, mean, err, name in zip(range(len(means)), means, errs, xaxis_names):
-        if len(name) > 1:
-            color = color_mappings.get(name[0:2], 'skyblue')
-        elif len(name) == 1:
-            color = color_mappings[name]
+        group_color = get_marker_color(name)
+        plt.errorbar(i, mean, yerr=err, fmt='o', color=group_color, alpha=0.8)
 
-        plt.errorbar(i, mean, yerr=err, fmt='o', color=color, alpha=0.8)
+    plt.title(feature, fontsize=10)
 
-    # names = [xaxis_mappings.get(name, name) for name in xaxis_names]
     label_fig_xaxis_names(xaxis_names)
 
 
@@ -78,7 +95,6 @@ def group_and_plot_by_type(data, group_by_col, feature, i, n, m):
     feature_means, feature_sems, lower_bounds, upper_bounds = extract_lower_and_upper_bounds(
         data, group_by_col, feature)
 
-    plt.style.use('fivethirtyeight')
     if len(group_by_col) == 2:
         letter_1 = group_by_col[0]
         letter_2 = group_by_col[-1]
@@ -100,13 +116,13 @@ def plot_all_graphs(data, features, letter_types):
         for i, letter_type in enumerate(letter_types):
             group_and_plot_by_type(data, letter_type, feature, i, 2, 2)
 
-        filename_to_save = '../MBTI_project/plots/' + feature.replace('.', '')
-        plt.savefig(filename_to_save, facecolor='whitesmoke')
+        filename_to_save = '{}/{}'.format(FUNCTION_PLOTS_PATH, feature.replace('.', ''))
+        plt.savefig(filename_to_save, facecolor='whitesmoke', bbox_inches="tight")
         plt.close()
 
-        group_and_plot_by_type(data, 'type', feature, 1, 1, 1)
-        filename_to_save = '../MBTI_project/plots/' + feature.replace('.', '') + '_all'
-        plt.savefig(filename_to_save, facecolor='whitesmoke')
+        group_and_plot_by_type(data, LABEL_COL, feature, 1, 1, 1)
+        filename_to_save = '{}/{}'.format(PLOTS_PATH, feature.replace('.', ''))
+        plt.savefig(filename_to_save, facecolor='whitesmoke', bbox_inches="tight")
         plt.close()
 
 
@@ -128,27 +144,26 @@ def compare_types(data, cols_to_group_by, all_types_to_compare, features):
                 letter_type_results[higher_key.format(max_ind[0])].append(feature)
 
         for letter_type in letter_types:
-            print('The following features are higher in {} type'.format(type_mappings.get(letter_type, letter_type)))
+            print('The following features are higher in {} type'.format(FUNCTION_MAPPINGS.get(letter_type, letter_type)))
             for feature in letter_type_results[higher_key.format(letter_type)]:
                 print(feature)
             print()
 
 
 if __name__ == "__main__":
-    path = '../MBTI_project/featurized_mbti'
-    data = pd.read_csv(path, encoding='utf-8')
+    featurized_data = pd.read_csv(FEATURIZED_PATH, encoding='utf-8').drop(['Unnamed: 0'], axis=1)
+    vectorized_data = pd.read_csv(VECTORIZED_PATH, encoding='utf-8').drop(['Unnamed: 0'], axis=1)
 
-    features = list(data.columns)
-    for field_to_remove in ['Unnamed: 0', 'type', 'posts']:
-        features.remove(field_to_remove)
+    data = pd.concat([featurized_data, vectorized_data], axis=1)
 
-    data['EI'] = [mbti_type[0] for mbti_type in data['type']]
-    data['NS'] = [mbti_type[1] for mbti_type in data['type']]
-    data['FT'] = [mbti_type[2] for mbti_type in data['type']]
-    data['JP'] = [mbti_type[3] for mbti_type in data['type']]
-    data['1_2'] = [mbti_type[0:2] for mbti_type in data['type']]
-    data['2_3'] = [mbti_type[1:3] for mbti_type in data['type']]
-    data['1_3_4'] = [mbti_type[0:1] + mbti_type[2:4] for mbti_type in data['type']]
+    features = list(featurized_data.columns) + TOP_WORDS
+    features.remove(LABEL_COL)
+
+    for function, function_inds in FUNCTION_INDS.items():
+        start_ind, stop_ind = function_inds
+        data[function] = [mbti_type[start_ind:stop_ind] for mbti_type in data[LABEL_COL]]
+
+    data['1_3_4'] = [mbti_type[0:1] + mbti_type[2:4] for mbti_type in data[LABEL_COL]]
 
     cols_to_group_by = ['EI', 'NS', 'FT', 'JP']
     all_types_to_compare = [['E', 'I'], ['N', 'S'], ['F', 'T'], ['J', 'P']]
