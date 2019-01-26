9e4d6fe4799f7257b514724a3392edfd1c516cbc
rismakov <rismakov@gmail.com>
2019-01-25 17:06:18 -0800

    added to constants file

--- utils/constants.py
+++ utils/constants.py
@@ -3,6 +3,8 @@ TFIDF_MAX_FEATURES = 1000
 FEATURIZED_PATH = 'data/featurized_data.csv'
 VECTORIZED_PATH = 'data/vectorized_data_{}.csv'.format(TFIDF_MAX_FEATURES)
 
+FEATURES_PATH = 'data/features_{}'.format(TFIDF_MAX_FEATURES)
+
 MODEL_COMPARISON_TABLE_FILE = 'model_comparison_table_{}'.format(TFIDF_MAX_FEATURES)
 
 PLOTS_PATH = 'plots/plots_by_type'
@@ -12,34 +14,38 @@ TEXT_COL = 'posts'
 LABEL_COL = 'type'
 
 ENNEAGRAM_TERMS = [
-	'1w2', '2w1', '2w3', '3w2', '3w4', '4w3', '4w5', 
-	'6w5', '6w7', '7w6', '7w8', '8w7', '8w9', '9w8', 
-	'so', 'sp', 'sx', 'enneagram', 'ennegram', 'socionics', 'tritype', 
-	'7s', '8s']
+  '1w2', '2w1', '2w3', '3w2', '3w4', '4w3', '4w5',
+  '6w5', '6w7', '7w6', '7w8', '8w7', '8w9', '9w8',
+  'so', 'sp', 'sx', 'enneagram', 'ennegram', 'socionics', 'tritype',
+  '7s', '8s'
+]
 
 FUNCTIONS = ['fe', 'fi', 'ne', 'ni', 'se', 'si', 'te', 'ti', 'sj', 'dom', 'sensing', 'perceiving']
 
 IMAGE_TERMS = ['gif', 'giphy', 'image', 'img', 'imgur', 'jpeg', 'jpg', 'JPG', 'photobucket', 'php', 'png', 'tinypic']
 
-INVALID_WORDS = ['im', 'mbti', 'functions', 'myers', 'briggs', 'types', 'type', 'personality', '16personalities', '16']
+INVALID_WORDS = [
+  'im', 'mbti', 'functions', 'myers', 'briggs', 'types', 'type',
+  'personality', '16personalities', '16', 'personalitycafe'
+]
 
-MBTI_PARTS = ['nt', 'nf', 'st', 'sf', 'sj', 
-			  'nts', 'nfs', 'sts', 'sfs', 'sjs', 
-			  'enxp', 'exfj', 'exfp', 'esxp',
-			  'ixfp', 'ixtp', 'ixtj', 'ixfj',
-			  'xntp', 'xnfj', 
-			  'nfp', 'ntp', 'ntj', 'nfp', 'sfj',
-			  'es', 'fj']
+MBTI_PARTS = [
+  'es', 'fj', 'nt', 'nf', 'st', 'sf', 'sj',
+  'nts', 'nfs', 'sts', 'sfs', 'sjs',
+  'enxp', 'exfj', 'exfp', 'esxp',
+  'ixfp', 'ixtp', 'ixtj', 'ixfj',
+  'xntp', 'xnfj',
+  'nfp', 'ntp', 'ntj', 'nfp', 'sfj',
+]
 
 MBTI_TYPES_UPPER = [
-    'ENTP', 'INTP', 'ENTJ', 'INTJ', 'ENFP', 'INFP', 'ENFJ', 'INFJ',
-    'ESTP', 'ISTP', 'ESTJ', 'ISTJ', 'ESFP', 'ISFP', 'ESFJ', 'ISFJ'
+  'ENFJ', 'ENFP', 'ENTJ', 'ENTP',
+  'ESFJ', 'ESFP', 'ESTJ', 'ESTP',
+  'INFJ', 'INFP', 'INTJ', 'INTP',
+  'ISFJ', 'ISFP', 'ISTJ', 'ISTP'
 ]
 
-MBTI_TYPES_LOWER = [
-    'entp', 'intp', 'entj', 'intj', 'enfp', 'infp', 'enfj', 'infj',
-    'estp', 'istp', 'estj', 'istj', 'esfp', 'isfp', 'esfj', 'isfj'
-]
+MBTI_TYPES_LOWER = [mbti_type.lower() for mbti_type in MBTI_TYPES_UPPER]
 
 MBTI_TYPES_PLURAL = [word + 's' for word in MBTI_TYPES_LOWER]
 
